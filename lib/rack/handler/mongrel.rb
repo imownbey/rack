59bf11fee68e3c3eba97dec5db1fb98e76c58698
Ian Ownbey <ian@inspir.es>
2009-01-31 19:19:49 -0800

    Made mongrel handler use new version of Mongrel#run

--- lib/rack/handler/mongrel.rb
+++ lib/rack/handler/mongrel.rb
@@ -5,10 +5,7 @@ module Rack
   module Handler
     class Mongrel < ::Mongrel::HttpHandler
       def self.run(app, options={})
-        server = ::Mongrel::HttpServer.new(options[:Host] || '0.0.0.0',
-                                           options[:Port] || 8080,
-                                           app)
-        server.start!
+        ::Mongrel.run(app, options)
       end
     end
   end
